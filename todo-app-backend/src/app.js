46c59790c22f6aa14290cbd6110ee96d82ef4b19
Hassan <malik.hr.2004@gmail.com>
2026-05-02 23:08:07 +0500

    Add start script for production app

--- todo-app-backend/src/app.js
+++ todo-app-backend/src/app.js
@@ -3,8 +3,19 @@ import cors from "cors";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    }
+    else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
 }));
 
 app.use(express.json({ limit: '16kb' })); // for json data
